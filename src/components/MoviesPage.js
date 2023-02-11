a443138208717694532735e7216ca1da5e80792a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-10 22:33:20 -0500

    import MovieShow and useRoutchMatch, render the returned url as part of the path

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
 
+// import the MovieShow component
+import MovieShow from "./MovieShow";
+
 function MoviesPage({ movies }) {
+
+  // useRouteMatch returns a special object with information about
+  // the currently matched route
+  const match = useRouteMatch();
+  console.log(match);  // {path: '/movies', url: '/movies', isExact: false, params: {…}}
+
   return (
     <div>
       <MoviesList movies={movies} />
+      {/* 
+        we can use the current URL from the `match` object as part of the path;
+        this will generate a url like "/movies/:movieId"
+      */}
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow />
+      </Route>
+
     </div>
   );
 }
